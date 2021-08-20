d7e429abc8403a0f4e1fea20c87e3fdbc9c13fe1
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2021-08-20 17:44:19 +0900

    header css edit

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -1,19 +1,21 @@
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
   ${reset};
   a{
-    text-decoration: none;
-    color: inherit;
+    text-decoration:none;
+    color:inherit;
   }
   *{
-    box-sizing: border-box;
+    box-sizing:border-box;
   }
   body{
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Canta;
-    font-size: 14px;
-    background-color: rgba(20,20,20,1);
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size:12px;
+    background-color:rgba(20, 20, 20, 1);
+    color:white;
+    padding-top:50px;
   }
 `;
 
