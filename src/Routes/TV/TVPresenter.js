2e047d9c4c9beb043b09abbeb0690d68379aaabf
guhyeon-kwon <ghkwon0130@gmail.com>
2021-08-30 23:42:42 +0900

    commit

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,8 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "../../Components/Section";
 
-const TVPresenter = () => null;
+const Container = styled.div``;
+
+const TVPresenter = ({
+                         topRated,
+                         popular,
+                         airingToday,
+                         loading,
+                         error
+                     }) => loading ? null :
+    <Container>
+        {topRated && topRated.length > 0 && (
+            <Section title="Popular Shows">{topRated.map(show => show.name)}</Section>
+        )}
+        {popular && popular.length > 0 && (
+        <Section title="Popular Shows">{popular.map(show => show.name)}</Section>
+        )}
+        {airingToday && airingToday.length > 0 && (
+            <Section title="Airing Today">{airingToday.map(show => show.name)}</Section>
+        )}
+    </Container>
 
 TVPresenter.prototypes = ({
     topRated: PropTypes.array,
